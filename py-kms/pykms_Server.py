0bf879874aa80a775a40658d20db2241b04c1d1f
ElevenNotes <github@11notes.ch>
2025-05-05 10:34:02 +0200

    [feature] add client IP to database

--- py-kms/pykms_Server.py
+++ py-kms/pykms_Server.py
@@ -482,7 +482,7 @@ def server_main_terminal():
 class kmsServerHandler(socketserver.BaseRequestHandler):
         def setup(self):
                 loggersrv.info("Connection accepted: %s:%d" %(self.client_address[0], self.client_address[1]))
-                srv_config['raddr'] = self.client_address
+                srv_config['raddr'] = str(self.client_address[0])
 
         def handle(self):
                 self.request.settimeout(srv_config['timeoutsndrcv'])
